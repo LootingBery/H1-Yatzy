1cc74c79df2a05e734a238d2b25054263eb90684
LootingBery <ryborg.benjamin@gmail.com>
2026-01-21 17:30:33 +0100

    bobby

--- Yatzy/Yatzy.cs
+++ Yatzy/Yatzy.cs
@@ -4,7 +4,21 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, Yatzy!");
+            Console.WriteLine("How many players? (1-4 players)");
+            string playerInput = Console.ReadLine();
+
+            if (!int.TryParse(playerInput, out int amountOfPlayers) || amountOfPlayers < 1 || amountOfPlayers > 4)
+            {
+                Console.WriteLine("Invalid input. 1 player by default.");
+                amountOfPlayers = 1;
+            }
+
+            Console.WriteLine("What is the name of players?");
+                List<string> playerNames = new List<string>();
+            for (int i = 1; i <= amountOfPlayers; i++)
+            {
+                playerNames.Add(playerInput);
+            }
         }
     }
 }
